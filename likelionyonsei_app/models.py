0ee9eea5ef55c0570a23ae6b28d9a0746bc064a4
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2022-08-04 17:14:15 +0900

    model

--- likelionyonsei_app/models.py
+++ likelionyonsei_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.PositiveIntegerField(default=1)
+    photo = models.ImageField(null=True, blank=True)
+    moto = models.CharField(max_length=100)
+    job = models.CharField(max_length=100)
+    detail1 = models.TextField()
+    detail2 = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Recruit(models.Model):
+    url = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.url
+
